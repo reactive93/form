da14e903cfb7e4cb0bfbb2db4cd7ae172f915da3
reactive93 <reactivegame@hotmail.com>
2017-12-09 14:07:40 +0300

    изменил html

--- src/main/java/com/form/web/WebController.java
+++ src/main/java/com/form/web/WebController.java
@@ -2,9 +2,19 @@ package com.form.web;
 
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class WebController {
+
+
+    @RequestMapping("/index")
+    public ModelAndView index(){
+
+        return new ModelAndView("index");
+    }
+
 
 }
